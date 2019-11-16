5990d383c1fcf9f0b904fd2cc33f0fb2947795de
Ybazli <yashar.bazli@gmail.com>
2019-11-16 09:31:35 +0300

    footer event and props

--- src/background.js
+++ src/background.js
@@ -1,10 +1,17 @@
 'use strict'
 
-import { app, protocol, BrowserWindow, systemPreferences, Tray } from 'electron'
+import {
+    app,
+    protocol,
+    BrowserWindow,
+    systemPreferences,
+    Tray,
+    Menu
+} from 'electron'
 
 import {
-	createProtocol,
-	installVueDevtools
+    createProtocol,
+    installVueDevtools
 } from 'vue-cli-plugin-electron-builder/lib'
 
 const path = require('path')
@@ -13,21 +20,30 @@ const path = require('path')
 const isDevelopment = process.env.NODE_ENV !== 'production'
 
 const icon = systemPreferences.isDarkMode()
-	? createIconFile('light-icon.png')
-	: createIconFile('dark-icon.png')
+    ? createIconFile('light-icon.png')
+    : createIconFile('dark-icon.png')
 
 //context menu
 
 // contextMenu({
-// 	prepend: (params, browserWindow) => [
-// 		{
-// 			label: 'Quick ✌️',
-// 			click: (menuItem, browserWindow, event) => {
-// 				app.quit()
-// 			}
-// 		}
-// 	]
+//     prepend: (params, browserWindow) => [
+//         {
+//             label: 'Quick ✌️',
+//             click: (menuItem, browserWindow, event) => {
+//                 app.quit()
+//             }
+//         }
+//     ]
 // })
+
+const contextMenu = Menu.buildFromTemplate([
+    {
+        label: 'Quick ✌️',
+        click: (menuItem, browserWindow, event) => {
+            app.quit()
+        }
+    }
+])
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
@@ -35,115 +51,121 @@ let win
 let tray = null
 // Scheme must be registered before the app is ready
 protocol.registerSchemesAsPrivileged([
-	{
-		scheme: 'app',
-		privileges: {
-			secure: true,
-			standard: true
-		}
-	}
+    {
+        scheme: 'app',
+        privileges: {
+            secure: true,
+            standard: true
+        }
+    }
 ])
 
 function createIconFile(str) {
-	return path.join(__dirname, '../src/assets/icon/', str)
+    return path.join(__dirname, '../src/assets/icon/', str)
 }
 
 function createWindow() {
-	// Create the browser window.
-	win = new BrowserWindow({
-		width: 300,
-		height: 400,
-		frame: false,
-		resizable: false,
-		webPreferences: {
-			nodeIntegration: true
-		}
-	})
+    // Create the browser window.
+    win = new BrowserWindow({
+        width: 300,
+        height: 400,
+        frame: false,
+        resizable: false,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    })
+    console.log(systemPreferences.isDarkMode())
 
-	tray = new Tray(icon)
+    tray = new Tray(icon)
 
-	//show tray when clicked on
-	tray.on('click', () => {
-		win.isVisible() ? win.hide() : win.show()
-	})
-	tray.setIgnoreDoubleClickEvents(true)
-	tray.on('right-click', () => {})
+    //show tray when clicked on
+    tray.on('click', () => {
+        win.isVisible() ? win.hide() : win.show()
+    })
+    tray.setIgnoreDoubleClickEvents(true)
+    tray.on('right-click', () => {
+        tray.popUpContextMenu(contextMenu)
+    })
 
-	win.on('show', () => {
-		tray.setHighlightMode('always')
-		const bounds = tray.getBounds()
-		win.setPosition(bounds.x - 120, bounds.y + 30)
-	})
+    win.on('show', () => {
+        tray.setHighlightMode('always')
+        const bounds = tray.getBounds()
+        win.setPosition(bounds.x - 120, bounds.y + 30)
+    })
+    win.on('blur', () => {
+        win.hide()
+    })
 
-	win.on('hide', () => {
-		tray.setHighlightMode('never')
-	})
+    win.on('hide', () => {
+        tray.setHighlightMode('never')
+    })
 
-	if (process.env.WEBPACK_DEV_SERVER_URL) {
-		// Load the url of the dev server if in development mode
-		win.loadURL(process.env.WEBPACK_DEV_SERVER_URL)
-		if (!process.env.IS_TEST) win.webContents.openDevTools()
-	} else {
-		createProtocol('app')
-		// Load the index.html when not in development
-		win.loadURL('app://./index.html')
-	}
+    if (process.env.WEBPACK_DEV_SERVER_URL) {
+        // Load the url of the dev server if in development mode
+        win.loadURL(process.env.WEBPACK_DEV_SERVER_URL)
+        if (!process.env.IS_TEST) win.webContents.openDevTools()
+    } else {
+        createProtocol('app')
+        // Load the index.html when not in development
+        win.loadURL('app://./index.html')
+    }
 
-	win.on('closed', () => {
-		win = null
-	})
+    win.on('closed', () => {
+        win = null
+    })
 }
 
 // Quit when all windows are closed.
 app.on('window-all-closed', () => {
-	// On macOS it is common for applications and their menu bar
-	// to stay active until the user quits explicitly with Cmd + Q
-	if (process.platform !== 'darwin') {
-		app.quit()
-	}
+    // On macOS it is common for applications and their menu bar
+    // to stay active until the user quits explicitly with Cmd + Q
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
 })
 
 app.on('activate', () => {
-	// On macOS it's common to re-create a window in the app when the
-	// dock icon is clicked and there are no other windows open.
-	if (win === null) {
-		createWindow()
-	}
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (win === null) {
+        createWindow()
+    }
 })
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.on('ready', async () => {
-	if (isDevelopment && !process.env.IS_TEST) {
-		// Install Vue Devtools
-		// Devtools extensions are broken in Electron 6.0.0 and greater
-		// See https://github.com/nklayman/vue-cli-plugin-electron-builder/issues/378 for more info
-		// Electron will not launch with Devtools extensions installed on Windows 10 with dark mode
-		// If you are not using Windows 10 dark mode, you may uncomment these lines
-		// In addition, if the linked issue is closed, you can upgrade electron and uncomment these lines
-		// try {
-		//   await installVueDevtools()
-		// } catch (e) {
-		//   console.error('Vue Devtools failed to install:', e.toString())
-		// }
-	}
-	createWindow()
+    if (isDevelopment && !process.env.IS_TEST) {
+        // Install Vue Devtools
+        // Devtools extensions are broken in Electron 6.0.0 and greater
+        // See https://github.com/nklayman/vue-cli-plugin-electron-builder/issues/378 for more info
+        // Electron will not launch with Devtools extensions installed on Windows 10 with dark mode
+        // If you are not using Windows 10 dark mode, you may uncomment these lines
+        // In addition, if the linked issue is closed, you can upgrade electron and uncomment these lines
+        // try {
+        //   await installVueDevtools()
+        // } catch (e) {
+        //   console.error('Vue Devtools failed to install:', e.toString())
+        // }
+    }
+    createWindow()
 })
 
 // Exit cleanly on request from parent process in development mode.
 if (isDevelopment) {
-	if (process.platform === 'win32') {
-		process.on('message', data => {
-			if (data === 'graceful-exit') {
-				app.quit()
-			}
-		})
-	} else {
-		process.on('SIGTERM', () => {
-			app.quit()
-		})
-	}
+    if (process.platform === 'win32') {
+        process.on('message', data => {
+            if (data === 'graceful-exit') {
+                app.quit()
+            }
+        })
+    } else {
+        process.on('SIGTERM', () => {
+            app.quit()
+        })
+    }
 }
 
 app.dock.hide()
--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,46 +1,42 @@
 <template>
-	<!-- fixed footer and buttons -->
-	<div
-		class="flex justify-between px-4 fixed inset-x-0 bottom-0 py-2 bg-gray-700"
-	>
-		<div class="font-light italic text-xs text-gray-500">
-			<span
-				class=" bg-gray-300 mr-1 rounded-full px-2 text-gray-900 font-semibold"
-			>
-				{{ length }}
-			</span>
-			Items
-		</div>
+    <!-- fixed footer and buttons -->
+    <div
+        class="flex justify-between px-4 fixed inset-x-0 bottom-0 py-2 bg-gray-700"
+    >
+        <div class="font-light italic text-xs text-gray-500">
+            <span
+                class="bg-gray-300 mr-1 rounded-full px-2 text-gray-900 font-semibold"
+                >{{ length }}</span
+            >
+            Items
+        </div>
 
-		<div class="text-sm">
-			{{ status }}
-		</div>
+        <div class="text-sm">{{ status }}</div>
 
-		<div class="font-light text-xs text-gray-500 ">
-			<button
-				@click="removeAll"
-				class="italic text-xs bg-gray-800 rounded-full px-2 text-white hover:bg-gray-900 focus:outline-none"
-			>
-				Clear All
-			</button>
-		</div>
-	</div>
-	<!-- end of footer -->
+        <div class="font-light text-xs text-gray-500">
+            <button
+                @click="removeAllEvent"
+                class="italic text-xs bg-gray-800 rounded-full px-2 text-white hover:bg-gray-900 focus:outline-none"
+                :disabled="this.length == 0"
+            >
+                Clear All
+            </button>
+        </div>
+    </div>
+    <!-- end of footer -->
 </template>
 
 <script>
 export default {
-	name: 'Footer',
-	data() {
-		return {
-			status: '',
-			length: 2
-		}
-	},
-	methods: {
-		removeAll() {
-			return true
-		}
-	}
+    name: 'Footer',
+    props: ['status', 'length'],
+    data() {
+        return {}
+    },
+    methods: {
+        removeAllEvent() {
+            this.$emit('removeAllEvent')
+        }
+    }
 }
 </script>
